0331321c63a7ab945660800a92bef84882bca689
yoshihide-saito <skyglowsy3@gmail.com>
2024-02-26 20:47:56 +0900

    todoアプリを作成

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Todo from './todo'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
 
